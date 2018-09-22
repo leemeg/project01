783427dee72bcd678d0f7f28fc0eac05cd4272bc
leemeg <marshalll@student.ncmich.edu>
2018-09-21 22:05:02 -0400

    added const

--- project01.js
+++ project01.js
@@ -8,9 +8,8 @@
 "use strict";
 const PROMPT = require('readline-sync');
 
-
-let LotNum, bed, bath, car, cost;
-
+let LotNum, nBed, nBath, nCar, cost;
+const base=50000,pBed=17000 ,pBath=12500 ,pCar=6000;
 
 function main() {
     setLotNum();// Lot Number
@@ -21,37 +20,29 @@ function main() {
     printResults();
 
 }
-
 main();
-
+function setcost() {
+    cost= base + nBed*pBed + nBath*pBath + nCar*pCar;
+}
+function printResults() {
+    process.stdout.write('\x1Bc'); //Clears the screen
+    console.log("\nThe cost of a new home in lot number " + LotNum + " of the River Falls subdivision, with " + nBed + " bedrooms, " + nBath + " bathrooms and a " + nCar + " car garage will be $" + cost);
+}
 function setLotNum() {
     LotNum = PROMPT.question(`\nPlease enter a River Falls lot number: `);
 }
-
 function setBedRooms() {
-    bed = PROMPT.question(`\nWhat will be the number of bedrooms in the home? `);
+    nBed = PROMPT.question(`\nWhat will be the number of bedrooms in the home? `);
 }
-
 function setBathRooms() {
-    bath = PROMPT.question(`\nHow many bathrooms will this new home have? `);
-
+    nBath = PROMPT.question(`\nHow many bathrooms will this new home have? `);
 }
 function setNumCars() {
-    car = PROMPT.question(`\nWhat will be the vehicle capacity of the garage? `);
-}
-
-function setcost() {
-    cost =50000+bed*17000+bath*12500+car*6000;
+    nCar = PROMPT.question(`\nWhat will be the vehicle capacity of the garage? `);
 }
 
 
-
-function printResults() {
-    process.stdout.write('\x1Bc'); //Clears the screen
-    console.log("\nThe cost of a new home in lot number " + LotNum + " of the River Falls subdivision, with " + bed + " bedrooms, " + bath + " bathrooms and a " + car + " car garage will be $" + cost);
-}
-
-/*
+/**
 This program is designed to demonstrate very basic coding paradigms
 Topics: code layout, declaring/initializing global/local variables/constants, parameters, modularization & dispatching
           comments (single/multi line, JSDoc), pragmas, data types
